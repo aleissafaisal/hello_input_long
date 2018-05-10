895d1b9ebde0e7800aa2f74c7a758b002c86621b
faisalaleissa <31824833+faisalaleissa@users.noreply.github.com>
2018-05-10 23:17:35 +0300

    c

---
+++ Hello_world.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+def print_my_name(name):
+	#sys.stdout.write(name)
+	arr = []
+	for item in range(100):
+		arr.append(name)
+	return arr
+
+listOfArguments = sys.argv[1:]
+Name = str(listOfArguments[0])
+
+sys.stdout.write(print_my_name(Name))
